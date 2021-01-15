74734d3e1e85431d8fd100bc3ab3b4af5749bf86
aoronchu <aoronchu@gmail.com>
2021-01-15 14:35:59 +0800

    init

---
+++ src/main/java/com/webank/authmanager/demo/SDKDemo.java
@@ -0,0 +1,52 @@
+package com.webank.authmanager.demo;
+
+import com.webank.authmanager.constant.AuthConstants;
+import com.webank.authmanager.contract.AuthManager;
+import com.webank.authmanager.factory.AuthManagerFactory;
+import com.webank.authmanager.service.AuthByAdminService;
+import com.webank.authmanager.utils.HashUtils;
+import org.fisco.bcos.sdk.BcosSDK;
+import org.fisco.bcos.sdk.client.Client;
+
+/**
+ * @author aaronchu
+ * @Description
+ * @data 2021/01/15
+ */
+public class SDKDemo {
+    public static void main(String[] args) throws Exception{
+        BcosSDK bcosSDK = BcosSDK.build("conf/config.toml");
+        Client client = bcosSDK.getClient(1);
+        AuthManagerFactory authManagerFactory = new AuthManagerFactory(client);
+        //Deploy contract
+        AuthManager authManager = authManagerFactory.createAdmin();
+        //Build facade
+        AuthByAdminService authByAdminService = new AuthByAdminService(authManager);
+        //Create group
+        String group = "badGroup";
+        authByAdminService.createGroup(group, AuthConstants.ACL_BLACKLIST_MODE);
+        String blackAccount = "0x01";
+        //Relate function
+        String bizContractAddress = "0x2";
+        String function = HashUtils.hash("add(uint256,uint256)");//业务函数签名
+        authByAdminService.addFunctionToGroup(bizContractAddress, function, group);
+        //Verify
+        boolean canCall = authByAdminService.canCallFunction(bizContractAddress, function, blackAccount);
+        System.out.println("Before blocking this account, can the account access the function?:"+canCall);
+
+        //Add black account
+        authByAdminService.addAccountToGroup(blackAccount, group);
+
+        //Verify
+        canCall = authByAdminService.canCallFunction(bizContractAddress, function, blackAccount);
+        System.out.println("After blocking this account, can the account access the function?:"+canCall);
+
+        //Remove black account
+        authByAdminService.removeAccountFromGroup(blackAccount, group);
+
+        //Verify
+        canCall = authByAdminService.canCallFunction(bizContractAddress, function, blackAccount);
+        System.out.println("After unblocking, can the account access the function?:"+canCall);
+        System.out.println("Demo finished with success");
+    }
+}
